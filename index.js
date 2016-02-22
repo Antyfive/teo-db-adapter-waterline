c7492ec4b18e30a5bd7ee56d8c72cca057715125
Andrew Teologov <teologov.and@gmail.com>
2016-02-22 21:58:27 +0200

    adapter scaffolding

---
+++ index.js
@@ -0,0 +1,11 @@
+/*!
+ * Teo.JS Waterline ORM Adapter
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 2/22/16
+ */
+
+"use strict";
+
+const WaterlineAdapter = require("./lib/teo.db.adapter.waterline");
+
+module.exports = WaterlineAdapter;
---
+++ test/common.js
@@ -0,0 +1,13 @@
+/*!
+ * Common
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 2/22/16
+ */
+
+"use strict";
+
+global.sinon = require("sinon");
+global.chai = require("chai");
+global.expect = global.chai.expect;
+global.assert = global.chai.assert;
+global.async = require("short-async");
